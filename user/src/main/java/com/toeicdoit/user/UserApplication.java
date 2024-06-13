a62634de3c694e1596159a6bdfc6bfe35447f472
seunghochoi9 <jackpot97912@gmail.com>
2024-06-13 12:17:20 +0900

    first commit

---
+++ user/src/main/java/com/toeicdoit/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.toeicdoit.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
---
+++ user/src/test/java/com/toeicdoit/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toeicdoit.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
