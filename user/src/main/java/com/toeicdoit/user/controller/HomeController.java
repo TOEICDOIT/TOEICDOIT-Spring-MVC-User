f53181267132ba774d1693cf3442a2c8e2ab83d5
seunghochoi9 <jackpot97912@gmail.com>
2024-06-13 12:33:54 +0900

    .

--- user/src/main/java/com/toeicdoit/user/controller/HomeController.java
+++ user/src/main/java/com/toeicdoit/user/controller/HomeController.java
@@ -3,11 +3,16 @@ package com.toeicdoit.user.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Hello User World! 안녕 유저 월드";
+
+        return "Hello User World! 안녕 유저 월드^^ "+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분"));
     }
 }
