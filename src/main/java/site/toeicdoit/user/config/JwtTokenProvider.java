7bf11343ed797f206648cf5f8314309412529031
seunghochoi9 <jackpot97912@gmail.com>
2024-06-27 17:55:01 +0900

    240627

--- src/main/java/site/toeicdoit/user/config/JwtTokenProvider.java
+++ src/main/java/site/toeicdoit/user/config/JwtTokenProvider.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
-
 import javax.crypto.SecretKey;
 import java.time.Instant;
 import java.util.Base64;
