71073472d31a7e020bc39cd352ad56d1b9b90eb9
seunghochoi9 <jackpot97912@gmail.com>
2024-08-14 13:16:04 +0900

    240814

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 show tables;
 
-insert into user_model (id, email, password, name, registration) values (1, 'admin@test.com', '1234', '관리자', 'LOCAL');
-insert into role_model (id, role, user_id) values (1, 1, 1);
+# insert into user_model (id, email, password, name, registration) values (1, 'admin@test.com', '1234', '관리자', 'LOCAL');
+# insert into role_model (id, role, user_id) values (1, 1, 1);
